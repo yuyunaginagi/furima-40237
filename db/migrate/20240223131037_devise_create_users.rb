c40424af6b4030440a2a0fe9d466ff88e0438149
yuyunaginagi <centrepg8@gmail.com>
2024-02-24 15:39:56 +0900

    カラムの追加と新規登録画面への遷移の実装

--- db/migrate/20240223131037_devise_create_users.rb
+++ db/migrate/20240223131037_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,               null: false, default: ""
+      t.string :encrypted_password,  null: false, default: ""
+      t.string :nickname,            null: false
+      t.string :first_name_kanji,    null: false
+      t.string :last_name_kanji,     null: false
+      t.string :first_name_katakana, null: false
+      t.string :last_name_katakana,  null: false
+      t.date   :birth_day,           null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_23_131037) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.string "first_name_kanji", null: false
+    t.string "last_name_kanji", null: false
+    t.string "first_name_katakana", null: false
+    t.string "last_name_katakana", null: false
+    t.date "birth_day", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
