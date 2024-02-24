a57ef6531366dbbc8f7f24fcc79d9a8bfdecac14
yuyunaginagi <centrepg8@gmail.com>
2024-02-24 15:47:32 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,            presence: true
+  validates :first_name_kanji,    presence: true
+  validates :last_name_kanji,     presence: true
+  validates :first_name_katakana, presence: true
+  validates :last_name_katakana,  presence: true
+  validates :birth_day,           presence: true
 end
