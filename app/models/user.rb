c6190ed6cde47e8aa1e10e5045a60051ad8f140a
yuyunaginagi <centrepg8@gmail.com>
2024-02-25 20:51:07 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
     validates :last_name
     validates :first_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
     validates :last_name_kana
     validates :first_name_kana
   end
