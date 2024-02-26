d8df8a825dec0d1134207b03f60fc2b3a295cd97
yuyunaginagi <centrepg8@gmail.com>
2024-02-26 23:08:02 +0900

    バリデーションの修正とエラーメッセージの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,13 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
+  belongs_to :user
 
-  validates :items_name, :string, presence: true
-  validates :explanation, :text, presence: true
-  validates :Category_id,        numericality: { other_than: 1 , message: "can't be blank" }
+  validates :items_name,  presence: true
+  validates :explanation, presence: true
+  validates :category_id,        numericality: { other_than: 1 , message: "can't be blank" }
   validates :condition_id,       numericality: { other_than: 1 , message: "can't be blank" }
   validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :prefecture_id,      numericality: { other_than: 1 , message: "can't be blank" }
   validates :number_of_day_id,   numericality: { other_than: 1 , message: "can't be blank" }
-  validates :price, :integer, presence: true
+  validates :price, format: { with: /\A\d+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
