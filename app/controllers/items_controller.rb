b6c21881ab5f5bec44119bb7ab5a43b2bf71a9a4
yuyunaginagi <centrepg8@gmail.com>
2024-03-02 17:01:14 +0900

    商品一覧表示を行うための記述全般

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    # @items = Item.all
+    @items = Item.all
   end
 
   def new
@@ -17,6 +17,10 @@ class ItemsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
