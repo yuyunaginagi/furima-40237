7c4077eb3c4fe3ed8718101f5d3884b23664752f
yuyunaginagi <centrepg8@gmail.com>
2024-03-08 21:46:41 +0900

    エラーハンドリングに関わる記述等

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -3,12 +3,14 @@ class OrderDelivery
   attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id
 
   with_options presence: true do
-  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Please enter a 10 or 11-digit phone number" }
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :city, presence: { message: "can't be blank" }
+  validates :addresses, presence: { message: "can't be blank" }
+  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input only number" }, length: { minimum: 10, message: "is too short" }
   validates :user_id
   validates :item_id
   end
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
