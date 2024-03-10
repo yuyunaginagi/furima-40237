8d891b36f8fc506aa574f6d7aa7f3d44e98f261f
yuyunaginagi <centrepg8@gmail.com>
2024-03-10 22:36:43 +0900

    挙動確認依頼後の修正

--- app/javascript/tax.js
+++ app/javascript/tax.js
@@ -1,13 +1,15 @@
 function tax (){
-  const itemPrice = document.getElementById("item-price");
-  itemPrice.addEventListener("keyup", () => {
-    const consumptionTax = Math.floor(itemPrice.value*0.1);
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+
     const addTaxPrice = document.getElementById("add-tax-price");
-    addTaxPrice.innerHTML = `${consumptionTax}`
-    const salesProfit = Math.floor(itemPrice.value*0.9);
+    const consumptionTax = Math.floor(inputValue * 0.1);
+    addTaxPrice.innerHTML = `${consumptionTax}`;
+
     const profit = document.getElementById("profit");
-    profit.innerHTML = `${salesProfit}`
+    const salesProfit = Math.floor(inputValue * 0.9);
+    profit.innerHTML = `${salesProfit}`;
   });
 };
 window.addEventListener('turbo:load', tax);
-
