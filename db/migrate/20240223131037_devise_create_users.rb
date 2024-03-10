a30749e1e6b2d2bf29b077ae55a40d0280a89832
yuyunaginagi <centrepg8@gmail.com>
2024-03-10 15:19:45 +0900

    テストコードの実装

--- db/migrate/20240223131037_devise_create_users.rb
+++ db/migrate/20240223131037_devise_create_users.rb
@@ -4,7 +4,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.references :user, null: false,  foreign_key: true
       t.string     :email,              null: false, default: ""
       t.string     :encrypted_password, null: false, default: ""
       t.string     :nickname,           null: false
